5bc52b2802c674149458379942210f3a3745e223
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-07-20 15:26:28 +0300

    add

--- src/components/views/Discussions/Discussions.js
+++ src/components/views/Discussions/Discussions.js
@@ -21,7 +21,7 @@
 
 import styles from './Discussions.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  } from "@fortawesome/free-brands-svg-icons";
+import { faComments } from "@fortawesome/free-solid-svg-icons";
 
 function Discussions() {
 
@@ -35,9 +35,39 @@ function Discussions() {
                 </div>
                 <div className={styles.commSections}>
                     <div className={styles.generalDisc}>
-                    <FontAwesomeIcon icon="fa-regular fa-comment" />
+                        <FontAwesomeIcon icon={faComments} className={styles.icon} />
+                        <div className={styles.column}>
+                            <p className={styles.heading}>General Discussions</p>
+                            <p className={styles.description}>This is the place to discuss it all. From an impending Nuclear War to equally important things like your favorite celebrity's toothbrush brand.</p>
+                        </div>
+                        <div className={styles.statistics}>
+                            <div className={styles.threads}>
+                                <p>Threads</p>
+                                <p className={styles.countP}>0</p>
+                            </div>
+                            <div className={styles.messages}>
+                                <p>Messages</p>
+                                <p className={styles.countP}>0</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.userReviews}></div>
+                    <div className={styles.originalWorks}>
+                        <FontAwesomeIcon icon={faComments} className={styles.icon} />
+                        <div className={styles.column}>
+                            <p className={styles.heading}>Original Works</p>
+                            <p className={styles.description}>This is the place to share the fruits of your creative bursts. Certain homemade videos does not count as creative work!</p>
+                        </div>
+                        <div className={styles.statistics}>
+                            <div className={styles.threads}>
+                                <p>Threads</p>
+                                <p className={styles.countP}>0</p>
+                            </div>
+                            <div className={styles.messages}>
+                                <p>Messages</p>
+                                <p className={styles.countP}>0</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
